dff2058f9b2ca442529eabdc75a99b6132139c39
CostaPedroHenrique <devpedrohenrique@gmail.com>
2020-12-27 21:00:35 -0300

    Requirements-dev

--- anjos_de_patas/settings.py
+++ anjos_de_patas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'pets',
     'adocoes',
 ]
@@ -71,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'anjos_de_patas.wsgi.application'
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
